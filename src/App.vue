f510453f27d743b2c62dc3528782ac41836f41b5
MariaFL <fedmaria2@gmail.com>
2021-03-23 21:59:03 +0500

    add task

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,18 @@
             color="primary"
             dark
     >
-      <v-btn to="/" tag="router-link">Home</v-btn> |
-      <router-link to="/lemurs" tag="v-btn">Lemurs</router-link>
+      <v-btn to="/" tag="router-link" color="primary">Home</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn to="/lemurs" tag="router-link" color="primary">
+        <v-img
+              alt="Лемуры"
+              class="shrink mt-1 hidden-sm-and-down"
+              contain
+              min-width="20"
+              src="./assets/lemur.png"
+              width="40"
+        />
+      </v-btn>
     </v-app-bar>
     <v-main>
       <router-view/>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,10 @@
 <template>
   <div>
-
-
-
-
-
       <div class="d-flex align-center">
         Кто самый популярный?
       </div>
 
-
-
-      <div class="d-flex align-center">
-        <a href="">
-          <v-img
-                  alt="Лемуры"
-                  class="shrink mt-1 hidden-sm-and-down"
-                  contain
-                  min-width="100"
-                  src="../assets/lemur.png"
-                  width="30"
-          />
-        </a>
-      </div>
       <Presentation/>
-
   </div>
 </template>
 
